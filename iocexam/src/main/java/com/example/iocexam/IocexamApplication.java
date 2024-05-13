b08dc1dd6b6cd901498c274c267757397a6d4806
Hxxjeong <heejeong3394@gmail.com>
2024-05-13 14:32:57 +0900

    Bean의 생명주기 관련 실습

--- iocexam/src/main/java/com/example/iocexam/IocexamApplication.java
+++ iocexam/src/main/java/com/example/iocexam/IocexamApplication.java
@@ -1,13 +1,17 @@
 package com.example.iocexam;
 
+import com.example.iocexam.controller.UserContoller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class IocexamApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(IocexamApplication.class, args);
+        ApplicationContext context = SpringApplication.run(IocexamApplication.class, args);
+        UserContoller userContoller = context.getBean(UserContoller.class);
+        userContoller.joinUser();
     }
 
 }
--- iocexam/src/main/java/com/example/iocexam/controller/UserContoller.java
+++ iocexam/src/main/java/com/example/iocexam/controller/UserContoller.java
@@ -2,6 +2,8 @@ package com.example.iocexam.controller;
 
 import com.example.iocexam.domain.User;
 import com.example.iocexam.service.UserService;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -9,9 +11,22 @@ public class UserContoller {
     private final UserService userService;
 
     public UserContoller(UserService userService) {
+        System.out.println("UserController의 생성자 호출");
         this.userService = userService;
     }
 
+    @PostConstruct
+    public void init() {
+        // 해당 Bean이 생성된 직후 호출
+        System.out.println("PostConstruct 실행");
+    }
+    
+    @PreDestroy
+    public void destroy() {
+        // Bean이 소멸되기 직전 호출
+        System.out.println("PreDestroy 실행");
+    }
+    
     public void joinUser() {
         // 실제 동작할 때는 사용자로부터 정보를 받아옴
         User user = new User();
