66a3a3692ac8ed5f9820ca341524a35fba8f3511
Hxxjeong <heejeong3394@gmail.com>
2024-05-16 16:31:27 +0900

    외부 SQL 파일 이용

---
+++ springjdbc/src/main/java/com/example/springjdbc06/SpringJdbcApplication06.java
@@ -0,0 +1,22 @@
+package com.example.springjdbc06;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringJdbcApplication06 {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJdbcApplication06.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner demo(UserDao userDao) {
+        return args -> {
+            User user = new User(null, "Esther", "esther@email.com");
+            userDao.createUser(user);
+            System.out.println("user 입력 완료");
+        };
+    }
+}
