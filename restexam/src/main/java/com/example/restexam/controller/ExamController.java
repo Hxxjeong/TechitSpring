51b5a85c6fbc00377b3e4af91dc709c4cbb4df8c
Hxxjeong <heejeong3394@gmail.com>
2024-06-04 16:21:53 +0900

    예외/에러 처리 실습

---
+++ restexam/src/main/java/com/example/restexam/controller/ExamController.java
@@ -0,0 +1,12 @@
+package com.example.restexam.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExamController {
+    @GetMapping("/api/error")
+    public String apiError() {
+        throw new RuntimeException("API Error");
+    }
+}
