dca7c9cc7f3e5ef38657776660c7be939831e519
Hxxjeong <heejeong3394@gmail.com>
2024-07-02 18:49:44 +0900

    JWT RefreshToken 실습

--- securityexam/src/main/java/com/example/jwtexam/controller/UserApiController.java
+++ securityexam/src/main/java/com/example/jwtexam/controller/UserApiController.java
@@ -5,10 +5,12 @@ import com.example.jwtexam.domain.RefreshToken;
 import com.example.jwtexam.domain.Role;
 import com.example.jwtexam.domain.User;
 import com.example.jwtexam.dto.UserLoginRspDto;
-import com.example.jwtexam.jwt.util.JwtTokenizer;
+import com.example.jwtexam.auth.jwt.util.JwtTokenizer;
 import com.example.jwtexam.service.RefreshTokenService;
 import com.example.jwtexam.service.UserService;
+import io.jsonwebtoken.Claims;
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +18,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -94,6 +97,68 @@ public class UserApiController {
 
         response.addCookie(accessTokenCookie);
         response.addCookie(refreshTokenCookie);
+
+        return new ResponseEntity(rspDto, HttpStatus.OK);
+    }
+
+    @GetMapping("/api/authtest")
+    public String authTest() {
+        return "auth test";
+    }
+
+    // refresh token 생성
+    @PostMapping("/refreshToken")
+    public ResponseEntity createRefreshToken(HttpServletRequest request,
+                                             HttpServletResponse response) {
+        String refreshToken = null;
+        Cookie[] cookies = request.getCookies();
+
+        // 쿠키로부터 refresh token 얻어오기
+        if(cookies != null) {
+            for(Cookie cookie: cookies) {
+                if("refreshToken".equals(cookie.getName())) {
+                    refreshToken = cookie.getValue();
+                    break;
+                }
+            }
+        }
+
+        // 쿠키가 없는 경우 예외 처리
+        if(refreshToken == null) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+        // 토큰으로부터 정보 얻기
+        Claims claims = jwtTokenizer.parseRefreshToken(refreshToken);
+        Long userId = Long.valueOf((Integer) claims.get("userId"));
+
+        User user = userService.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("사용자를 찾지 못하였습니다."));
+
+        // access token 생성
+        List roles = (List) claims.get("roles");
+
+        String accessToken = jwtTokenizer.createAccessToken(
+                userId,
+                user.getEmail(),
+                user.getName(),
+                user.getUsername(),
+                roles
+        );
+
+        // 쿠키에 토큰 저장
+        Cookie accessTokenCookie = new Cookie("accessToken", accessToken);
+        accessTokenCookie.setHttpOnly(true);
+        accessTokenCookie.setPath("/");
+        accessTokenCookie.setMaxAge(Math.toIntExact(JwtTokenizer.ACCESS_TOKEN_EXPIRE_COUNT / 1000));
+
+        response.addCookie(accessTokenCookie);
+
+        // 응답 객체
+        UserLoginRspDto rspDto = UserLoginRspDto.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .userId(userId)
+                .name(user.getName())
+                .build();
 
         return new ResponseEntity(rspDto, HttpStatus.OK);
     }
